127a7fc077b13c38eeab4f04a8b2880278ca85e3
SantigoKid <santiagov123xd@gmail.com>
2022-11-11 13:45:20 +0100

    commit

--- Fronted/JavaScript/my-hangman-game/script.js
+++ Fronted/JavaScript/my-hangman-game/script.js
@@ -1,13 +1,44 @@
 const palabras = [
-"funcion",
-"declarar",
-"else",
-"let",
-"if",
-"imprimir",
-"array"
+    "funcion",
+    "declarar",
+    "else",
+    "let",
+    "if",
+    "imprimir",
+    "array"
 ]
 
-let displayPalabra = document.getElementById('palabra')
+let displayPalabra = document.getElementById('palabra');
 
-let random = Math.random 
+let random = Math.random();
+
+random = Math.random() * palabras.length;
+
+random = Math.floor(Math.random() * palabras.length);
+
+let palabra = palabras[random];
+
+let longitud = palabra.length;
+// console.log(longitud);
+
+// Procedo qa ocultar las palabras intercambiando el texto por guiones
+let texto = ""
+for (let index = 0; index < longitud; index++);
+
+texto = "_";
+
+displayPalabra.innerHTML = texto;
+
+// procedo a hacer funcionar los botones
+
+const botones = document.getElementById('tablero').childNodes;
+
+
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", juego)
+}
+
+// Procedo a hacer que el juego funcione
+function juego() {
+    console.log('Pulsaste un botón')
+}
