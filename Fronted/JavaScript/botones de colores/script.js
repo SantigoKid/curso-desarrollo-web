96f2ec2d313e6374d333b6b6d1132e71bae1c646
SantigoKid <santiagov123xd@gmail.com>
2022-11-24 13:59:11 +0100

    first commit

---
+++ Fronted/JavaScript/botones de colores/script.js
@@ -0,0 +1,12 @@
+// Función avanzada
+// Seleccionar los botones
+
+
+// Función sencilla
+function colorChange(color) {
+    document.body.style.background = color;
+    console.log('has pulsado un botón');
+}
+function colorReset() {
+    document.body.style.background = 'linear-gradient(90deg,rgb(41, 56, 97), black)';
+}
